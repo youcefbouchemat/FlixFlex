8c09fee00b4dba28a293e50b4361bc8df5fc439d
apple <apple@MacBook-Pro.local>
2023-01-24 20:17:31 +0100

    add dots to trending movies

--- src/components/TrendingMovies.tsx
+++ src/components/TrendingMovies.tsx
@@ -3,11 +3,12 @@ import React, {useEffect, useRef, useState} from 'react';
 import axios from 'axios';
 import axiosInstance from '../../config/axios';
 import TrendingMovieCard from './TrendingMovieCard';
+import colors from '../../assets/colors';
 
 const TrendingMovies = () => {
   const newMovieScrollX = useRef(new Animated.Value(0)).current;
-
   const windowWidth = Dimensions.get('window').width;
+  const dotposition = Animated.divide(newMovieScrollX, windowWidth);
 
   const [trendingMovies, setTrendingMovies] = useState([]);
 
@@ -35,30 +36,71 @@ const TrendingMovies = () => {
   }, []);
 
   return (
-    <Animated.FlatList
-      horizontal
-      pagingEnabled
-      snapToAlignment={'center'}
-      snapToInterval={windowWidth}
-      showsHorizontalScrollIndicator={false}
-      scrollEventThrottle={16}
-      decelerationRate={0}
-      contentContainerStyle={styles.flatlistContainer}
-      data={trendingMovies}
-      keyExtractor={item => item.id}
-      onScroll={Animated.event(
-        [{nativeEvent: {contentOffset: {x: newMovieScrollX}}}],
-        {useNativeDriver: false},
-      )}
-      renderItem={({item, index}) => {
-        return (
-          <TrendingMovieCard
-            title={item.original_title}
-            image={item.backdrop_path}
-          />
-        );
-      }}
-    />
+    <>
+      <Animated.FlatList
+        horizontal
+        pagingEnabled
+        snapToAlignment={'center'}
+        snapToInterval={windowWidth}
+        showsHorizontalScrollIndicator={false}
+        scrollEventThrottle={16}
+        decelerationRate={0}
+        contentContainerStyle={styles.flatlistContainer}
+        data={trendingMovies}
+        keyExtractor={item => item.id}
+        onScroll={Animated.event(
+          [{nativeEvent: {contentOffset: {x: newMovieScrollX}}}],
+          {useNativeDriver: false},
+        )}
+        renderItem={({item, index}) => {
+          return (
+            <TrendingMovieCard
+              title={item.original_title}
+              image={item.backdrop_path}
+            />
+          );
+        }}
+      />
+      <View style={styles.dotContainer}>
+        {trendingMovies.map((item, index) => {
+          const opacity = dotposition.interpolate({
+            inputRange: [index - 1, index, index + 1],
+            outputRange: [0.3, 1, 0.3],
+            extrapolate: 'clamp',
+          });
+
+          const dotWidth = dotposition.interpolate({
+            inputRange: [index - 1, index, index + 1],
+            outputRange: [6, 20, 6],
+            extrapolate: 'clamp',
+          });
+
+          const dotColor = dotposition.interpolate({
+            inputRange: [index - 1, index, index + 1],
+            outputRange: [
+              colors.placeholderText,
+              colors.secondaryBackground,
+              colors.placeholderText,
+            ],
+            extrapolate: 'clamp',
+          });
+
+          return (
+            <Animated.View
+              key={`dot-${index}`}
+              style={{
+                borderRadius: 12,
+                marginHorizontal: 3,
+                width: dotWidth,
+                opacity: opacity,
+                height: 6,
+                backgroundColor: dotColor,
+              }}
+            />
+          );
+        })}
+      </View>
+    </>
   );
 };
 
@@ -67,5 +109,11 @@ export default TrendingMovies;
 const styles = StyleSheet.create({
   flatlistContainer: {
     marginTop: 12,
+  },
+  dotContainer: {
+    flexDirection: 'row',
+    marginTop: 15,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
