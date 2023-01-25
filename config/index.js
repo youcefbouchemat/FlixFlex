d2628640091d4c64df70c377309b69f40fba18d4
apple <apple@MacBook-Pro.local>
2023-01-25 16:20:25 +0100

    getting similar movies

--- config/index.js
+++ config/index.js
@@ -1,2 +1,3 @@
 export const imageBaseUrl = 'https://image.tmdb.org/t/p/original/';
 export const baseUrl = 'https://api.themoviedb.org/3/';
+export const youtubeUrl = 'https://www.youtube.com/watch?v=';
--- src/screens/MovieDetails.tsx
+++ src/screens/MovieDetails.tsx
@@ -1,19 +1,26 @@
 import {
+  Dimensions,
+  FlatList,
   SafeAreaView,
   StyleSheet,
   Text,
   TouchableOpacity,
   View,
 } from 'react-native';
-import React from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import colors from '../../assets/colors';
 import {Image} from 'react-native';
-import {imageBaseUrl} from '../../config';
+import {imageBaseUrl, youtubeUrl} from '../../config';
 import {SharedElement} from 'react-navigation-shared-element';
 import {ScrollView} from 'react-native';
 import {Icon} from '@rneui/themed';
+import axios from 'axios';
+import axiosInstance from '../../config/axios';
+import YoutubeIframe from 'react-native-youtube-iframe';
 
 const MovieDetails = ({route}) => {
+  const windowWidth = Dimensions.get('window').width;
+
   const id = route.params.data.id;
   const title = route.params.data.original_title;
   const overview = route.params.data.overview;
@@ -22,6 +29,72 @@ const MovieDetails = ({route}) => {
   const releaseDate = route.params.data.release_date;
   const note = route.params.data.vote_average;
   const voteCount = route.params.data.vote_count;
+
+  const [youtubeKey, setYoutubeKey] = useState('');
+  const [trailerIsPlaying, setTrailerIsPlaying] = useState(false);
+
+  const [similarMoviesPage, setSimilarMoviesPage] = useState(1);
+  const [similarMovies, setSimilarMovies] = useState([]);
+
+  useEffect(() => {
+    const source = axios.CancelToken.source();
+    axiosInstance
+      .get(
+        `movie/${id}/videos?api_key=b488ff37b7bcb45680153eab3372c68e&language=en-US`,
+        {
+          cancelToken: source.token,
+        },
+      )
+      .then(response => {
+        setYoutubeKey(response.data.results[0].key);
+      })
+      .catch(error => {
+        if (axios.isCancel(error)) {
+          console.log('canceled', error);
+        } else {
+          console.log('error in getting trailer', error);
+        }
+      });
+
+    return () => {
+      source.cancel();
+    };
+  }, [id]);
+
+  useEffect(() => {
+    const source = axios.CancelToken.source();
+    axiosInstance
+      .get(
+        `movie/${id}/similar?api_key=b488ff37b7bcb45680153eab3372c68e&language=en-US&page=${similarMoviesPage}`,
+        {
+          cancelToken: source.token,
+        },
+      )
+      .then(response => {
+        setSimilarMovies(similarMovies.concat(response.data.results));
+      })
+      .catch(error => {
+        if (axios.isCancel(error)) {
+          console.log('canceled', error);
+        } else {
+          console.log('error in getting similare movies', error);
+        }
+      });
+
+    return () => {
+      source.cancel();
+    };
+  }, [id, similarMoviesPage]);
+
+  const onStateChange = useCallback(state => {
+    if (state === 'ended') {
+      setTrailerIsPlaying(false);
+    } else if (state === 'playing') {
+      setTrailerIsPlaying(true);
+    } else if (state === 'paused') {
+      setTrailerIsPlaying(false);
+    }
+  }, []);
 
   return (
     <SafeAreaView style={styles.container}>
@@ -58,6 +131,37 @@ const MovieDetails = ({route}) => {
         <View style={styles.overviewContainer}>
           <Text style={styles.overviewTitle}>Overview</Text>
           <Text style={styles.overviewText}>{overview}</Text>
+        </View>
+
+        <View style={styles.trailerContainer}>
+          <Text style={styles.trailerText}>Trailer</Text>
+          {youtubeKey == '' ? (
+            <Text style={styles.noAvailableTrailer}>No Available Trailer</Text>
+          ) : (
+            <YoutubeIframe
+              height={500}
+              width={windowWidth * 0.9}
+              play={trailerIsPlaying}
+              videoId={youtubeUrl + youtubeKey}
+              onChangeState={onStateChange}
+            />
+          )}
+        </View>
+        <View>
+          <Text
+            onPress={() => {
+              setSimilarMoviesPage(similarMoviesPage + 1);
+            }}>
+            Similar Movies
+          </Text>
+          <FlatList
+            data={similarMovies}
+            horizontal
+            keyExtractor={item => item.id}
+            renderItem={({item, index}) => {
+              return <Text key={index}> {item.original_title}</Text>;
+            }}
+          />
         </View>
       </ScrollView>
     </SafeAreaView>
@@ -106,5 +210,14 @@ const styles = StyleSheet.create({
   overviewContainer: {},
   overviewTitle: {},
   overviewText: {},
+  trailerContainer: {
+    alignItems: 'center',
+    backgroundColor: 'red',
+  },
+  trailerText: {
+    alignSelf: 'flex-start',
+    padding: 16,
+  },
+  noAvailableTrailer: {},
 });
 export default MovieDetails;
